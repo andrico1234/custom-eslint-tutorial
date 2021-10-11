ed1304d3369d4160803960a5c80117da2d046d97
Andrico <17087167+andrico1234@users.noreply.github.com>
2021-10-11 21:07:34 +0100

    initial commit

---
+++ tests/lib/rules/no-inline-styles.js
@@ -0,0 +1,34 @@
+const rule = require("../../../lib/rules/no-inline-styles");
+const RuleTester = require("eslint").RuleTester;
+
+const ruleTester = new RuleTester();
+
+ruleTester.run("no-inline-styles", rule, {
+  valid: ['html`<div class="fake-name"></div>`;'],
+  invalid: [
+    {
+      code: 'html`<div style=""></div>`;',
+      errors: [
+        {
+          message: "no-inline-style",
+        },
+      ],
+    },
+    {
+      code: 'html`<div style="display:none;"></div>`',
+      errors: [
+        {
+          message: "no-inline-style",
+        },
+      ],
+    },
+    {
+      code: 'html`<div style="${val}""></div>`',
+      errors: [
+        {
+          message: "no-inline-style",
+        },
+      ],
+    },
+  ],
+});
