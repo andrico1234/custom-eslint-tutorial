302a1f4b0fbe65fa555d815dc749fb8dd9bc35e9
Andrico <andrico.karoulla@healthhero.com>
2022-01-03 16:12:05 +0000

    complete tests

--- tests/lib/rules/no-interactive-tooltips.js
+++ tests/lib/rules/no-interactive-tooltips.js
@@ -11,13 +11,40 @@ const ruleTester = new RuleTester({
 ruleTester.run("no-interactive-tooltips", rule, {
   valid: [
     'html`<simba-tooltip><span slot="content"> Error! </span></simba-tooltip>`',
+    'html`<simba-tooltip><span slot="content"></span></simba-tooltip>`',
+    'html`<simba-tooltip><span slot="content"><p>Error!</p></span></simba-tooltip>`',
+    'html`<simba-tooltip><span slot="content"><ul><li>Error!</li></ul></span></simba-tooltip>`',
   ],
   invalid: [
     {
-      code: "",
+      code: 'html`<simba-tooltip><span slot="content"><button> Error! </button></span></simba-tooltip>`',
       errors: [
         {
-          message: "",
+          message: "no-interactive-tooltips",
+        },
+      ],
+    },
+    {
+      code: 'html`<simba-tooltip><span slot="content"><div><button> Error! </button></div></span></simba-tooltip>`',
+      errors: [
+        {
+          message: "no-interactive-tooltips",
+        },
+      ],
+    },
+    {
+      code: 'html`<simba-tooltip><span slot="content"><div tabindex={1}> Error! </div></span></simba-tooltip>`',
+      errors: [
+        {
+          message: "no-interactive-tooltips",
+        },
+      ],
+    },
+    {
+      code: 'html`<simba-tooltip><span slot="content"><div><div tabindex={1}> Error! </div><div></span></simba-tooltip>`',
+      errors: [
+        {
+          message: "no-interactive-tooltips",
         },
       ],
     },
